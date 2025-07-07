655b531923f7a765d765afd86dcd256f8b33f8b1
pujitha15 <svspuji007@gmail.com>
2025-07-08 00:23:39 +0530

    updated logic of computer choice

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ let compScore = 0;
 let humanScore = 0;
 let round = 1;
 
+const options = ['rock', 'paper', 'scissors'];
+
 const getComputerChoice = () => {
     const rand = Math.random();
-    if(rand == 0) return "rock";
-    else if(rand > 0 && rand <= 0.5) return "paper";
-    return "scissors";
+    const index = Math.floor(rand * 3);
+    return options[index];
 }
 
 const getHumanChoice = () => {
